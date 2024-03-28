6b4189ccdb0ce3c5067d99ed4fbd13d464d4c182
GogoGit <6920876+GogoGit@users.noreply.github.com>
2024-03-28 18:26:55 -0400

    Instructor made a trap and I fell into it!  Solution is : useEffect(() => {    fetch(`http://localhost:3001/pirates`)       {...}   }, []); // notice the [] I added here.

--- pirates/src/App.js
+++ pirates/src/App.js
@@ -1,7 +1,8 @@
 import Header from "./components/Header";
 import Pirate from "./components/Pirate";
 import piratesFile from "./data/sample-pirates-array";
-import React from "react";
+// import React from "react";
+import React, { userState, useEffect } from "react";
 import AddPirate from "./components/AddPirate";
 
 // import React from "react";     //Not needed.
@@ -27,8 +28,35 @@ const pirateCalls = [
 const randomize = () =>
   pirateCalls[Math.floor(Math.random() * pirateCalls.length)];
 
+function passPirateData(data) {
+  return data;
+}
+
 function App() {
   const [pirates, setPirates] = React.useState(piratesFile);
+  // let [pirates, setPirates] = React.useState(piratesFile);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/pirates`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.length);
+        /*This didn't work:  setPirates=data;
+         Probably due to the fact that this is a REACT Component??? So confusing???
+         
+         */
+        setPirates(data);
+      })
+      .catch((err) => console.error(`Fetch problem: ${err.message}`));
+  }, []);
+
+  console.log(`after const`);
+  console.log(pirates);
 
   //This is common pattern, you make a copy of the data, then you modify it and then you update the original data with the new data.
   // const addPirate = (pirate) => {
@@ -52,7 +80,6 @@ function App() {
     <div>
       <Header title={randomize()} />
       <AddPirate addPirate={addPirate} />
-
       {/* {piratesFile.map((pirate, index) => ( */}
       {pirates.map((pirate, index) => (
         // <Pirate key={index} name={pirate.name} tagline={randomize()} />
