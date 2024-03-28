0d89836ffc3b361754113355014af18628949bbb
GogoGit <6920876+GogoGit@users.noreply.github.com>
2024-03-28 19:19:30 -0400

    Updated Pirage Add Form

--- pirates/src/components/AddPirate.js
+++ pirates/src/components/AddPirate.js
@@ -1,34 +1,31 @@
 import "../assets/css/AddPirateForm.css";
 import React from "react";
+import Button from "./Button";
 
 const AddPirate = ({ addPirate }) => {
   const [name, setName] = React.useState("");
   const [vessel, setVessel] = React.useState("");
   const [weapon, setWeapon] = React.useState("");
-
+  const [death, setDeath] = React.useState("");
+  const [desc, setDesc] = React.useState("");
   //Can't be outside of AddPrite as the variables would be out of scope... forgot error message something about global variables
-  function createPirate(event) {
-    event.preventDefault(); //Stops the page from refeshing!
-    const pirate = {
-      // Short Form
-      // const pirate = {
-      //   name,
-      //   vessel,
-      //   weapon,
-      // };
+  const createPirate = (event) => {
+    event.preventDefault();
 
-      // Long Form
+    const pirate = {
       name: name,
       vessel: vessel,
       weapon: weapon,
+      death: death,
+      desc: desc,
     };
-
     addPirate(pirate);
-
     setName("");
     setVessel("");
     setWeapon("");
-  }
+    setDeath("");
+    setDesc("");
+  };
 
   return (
     <form onSubmit={createPirate}>
@@ -56,7 +53,25 @@ const AddPirate = ({ addPirate }) => {
         value={weapon}
         onChange={(event) => setWeapon(event.target.value)}
       />
-      <button type="submit">Add Pirate</button>
+      <label htmlFor="died">Died</label>
+      <input
+        id="died"
+        type="text"
+        placeholder="Date of death"
+        value={death}
+        onChange={(event) => setDeath(event.target.value)}
+      />
+      <label htmlFor="desc">Description</label>
+      <textarea
+        id="desc"
+        placeholder="Pirate description"
+        value={desc}
+        onChange={(event) => setDesc(event.target.value)}
+      />
+      {/* replacing button */}
+      {/* <button type="submit">Add Pirate</button> */}
+      {/* use the Button component */}
+      <Button text={"Add Pirate"} />
     </form>
   );
 };
