5caee70e4057b6b5f063234bdec90890b89b73fe
GogoGit <6920876+GogoGit@users.noreply.github.com>
2024-03-28 19:48:09 -0400

    created a way to handle the input of the form and streamline it.

--- pirates/src/components/AddPirate.js
+++ pirates/src/components/AddPirate.js
@@ -3,30 +3,35 @@ import React from "react";
 import Button from "./Button";
 
 const AddPirate = ({ addPirate }) => {
-  const [name, setName] = React.useState("");
-  const [vessel, setVessel] = React.useState("");
-  const [weapon, setWeapon] = React.useState("");
-  const [death, setDeath] = React.useState("");
-  const [desc, setDesc] = React.useState("");
+  const initialFormState = {
+    name: "",
+    vessel: "",
+    weapon: "",
+    year: "",
+    desc: "",
+  };
+
+  const [pirate, setPirate] = React.useState(initialFormState);
+
+  // const [name, setName] = React.useState("");
+  // const [vessel, setVessel] = React.useState("");
+  // const [weapon, setWeapon] = React.useState("");
+  // const [death, setDeath] = React.useState("");
+  // const [desc, setDesc] = React.useState("");
+
+  // This is how to collect the data that has changed in ONE PLACE
+  // THIS is a good Pattern!!!
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setPirate({ ...pirate, [name]: value });
+  };
+
   //Can't be outside of AddPrite as the variables would be out of scope... forgot error message something about global variables
   const createPirate = (event) => {
     event.preventDefault();
-
-    const pirate = {
-      name: name,
-      vessel: vessel,
-      weapon: weapon,
-      death: death,
-      desc: desc,
-    };
     addPirate(pirate);
-    setName("");
-    setVessel("");
-    setWeapon("");
-    setDeath("");
-    setDesc("");
+    setPirate(initialFormState);
   };
-
   return (
     <form onSubmit={createPirate}>
       <label htmlFor="name">Name</label>
@@ -34,43 +39,49 @@ const AddPirate = ({ addPirate }) => {
         id="name"
         type="text"
         placeholder="Pirate name"
-        value={name}
-        onChange={(event) => setName(event.target.value)}
+        name="name"
+        value={pirate.name}
+        onChange={handleInputChange}
       />
       <label htmlFor="vessel">Vessel</label>
       <input
         id="vessel"
         type="text"
         placeholder="Pirate vessel"
-        value={vessel}
-        onChange={(event) => setVessel(event.target.value)}
+        name="vessel"
+        value={pirate.vessel}
+        onChange={handleInputChange}
       />
       <label htmlFor="weapon">Weapon</label>
       <input
         id="weapon"
         type="text"
+        name="weapon"
         placeholder="Pirate weapon"
-        value={weapon}
-        onChange={(event) => setWeapon(event.target.value)}
+        value={pirate.weapon}
+        onChange={handleInputChange}
       />
       <label htmlFor="died">Died</label>
       <input
         id="died"
         type="text"
-        placeholder="Date of death"
-        value={death}
-        onChange={(event) => setDeath(event.target.value)}
+        name="year"
+        placeholder="Year of death"
+        value={pirate.year}
+        onChange={handleInputChange}
       />
       <label htmlFor="desc">Description</label>
       <textarea
         id="desc"
+        name="desc"
         placeholder="Pirate description"
-        value={desc}
-        onChange={(event) => setDesc(event.target.value)}
+        value={pirate.desc}
+        onChange={handleInputChange}
       />
       {/* replacing button */}
       {/* <button type="submit">Add Pirate</button> */}
       {/* use the Button component */}
+
       <Button text={"Add Pirate"} />
     </form>
   );
